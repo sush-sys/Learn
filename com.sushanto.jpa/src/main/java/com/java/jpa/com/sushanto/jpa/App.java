8a97f95d0268048efd0255ca9d2b84f512e5f8c1
Sushant <sushantogorain98@gmail.com>
2022-03-03 00:47:11 +0530

    modify

--- com.sushanto.jpa/src/main/java/com/java/jpa/com/sushanto/jpa/App.java
+++ com.sushanto.jpa/src/main/java/com/java/jpa/com/sushanto/jpa/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	int a=10;
+    	int b=20;
+        System.out.println( "sum ="+(a+b));
     }
 }
