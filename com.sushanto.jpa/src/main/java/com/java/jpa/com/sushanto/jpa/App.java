aac41bb9d189b0440268fc701bca30e61549feca
Sushant <sushantogorain98@gmail.com>
2022-03-03 15:11:38 +0530

    multiply of numbers

--- com.sushanto.jpa/src/main/java/com/java/jpa/com/sushanto/jpa/App.java
+++ com.sushanto.jpa/src/main/java/com/java/jpa/com/sushanto/jpa/App.java
@@ -11,5 +11,8 @@ public class App
     	int a=12;
     	int b=20;
         System.out.println( "sum ="+(a+b));
+        Multiple res=new Multiple();
+        int ans=res.multiply(7, 8);
+        System.out.println("Multiply =" + ans);
     }
 }
---
+++ com.sushanto.jpa/src/main/java/com/java/jpa/com/sushanto/jpa/Multiple.java
@@ -0,0 +1,14 @@
+package com.java.jpa.com.sushanto.jpa;
+
+public class Multiple {
+	
+	int multiply(int a, int b)
+	{
+		int res=a;
+		res=res * b;
+		return res;
+	}
+	
+	
+
+}
