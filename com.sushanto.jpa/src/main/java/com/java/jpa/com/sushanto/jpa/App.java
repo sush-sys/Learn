6d74aff9f8bcc064d6c71f66b981256d33e57fd5
Sushant <sushantogorain98@gmail.com>
2022-03-03 11:06:21 +0530

    4th commit

--- com.sushanto.jpa/src/main/java/com/java/jpa/com/sushanto/jpa/App.java
+++ com.sushanto.jpa/src/main/java/com/java/jpa/com/sushanto/jpa/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	int a=10;
+    	int a=12;
     	int b=20;
         System.out.println( "sum ="+(a+b));
     }
---
+++ com.sushanto.jpa/src/main/java/com/java/jpa/com/sushanto/jpa/Sum.java
@@ -0,0 +1,10 @@
+package com.java.jpa.com.sushanto.jpa;
+
+public class Sum {
+	
+	int sum (int a,int b)
+	{
+		return (a+b);
+	}
+
+}
